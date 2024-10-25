23147ebe033eecd8976eb3232bc54194675bff6a
Nguyễn Kiều Anh Quân <168531710+CharleSs0933@users.noreply.github.com>
2024-10-25 15:13:57 +0700

    Create Shopping Order Reducer

---
+++ client/src/store/shop/order-slice/index.js
@@ -0,0 +1,46 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  approvalURL: null,
+  isLoading: false,
+  orderId: null,
+};
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+export const createNewOrder = createAsyncThunk(
+  "order/createNewOrder",
+  async (orderData) => {
+    const result = await axios.post(
+      `${apiUrl}/api/shop/order/create`,
+      orderData
+    );
+
+    return result?.data;
+  }
+);
+
+const shoppingOrderSlice = createSlice({
+  name: "shoppingOrder",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createNewOrder.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createNewOrder.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.approvalURL = action.payload.approvalURL;
+        state.orderId = action.payload.orderId;
+      })
+      .addCase(createNewOrder.rejected, (state, action) => {
+        state.isLoading = false;
+        state.approvalURL = null;
+        state.orderId = null;
+      });
+  },
+});
+
+export default shoppingOrderSlice.reducer;
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -4,6 +4,7 @@ import adminProductsSlice from "./admin/product-slice";
 import shoppingProductsSlice from "./shop/product-slice";
 import shoppingCartSlice from "./shop/cart-slice";
 import shoppingAddressSlice from "./shop/address-slice";
+import shoppingOrderSlice from "./shop/order-slice";
 
 const store = configureStore({
   reducer: {
@@ -12,6 +13,7 @@ const store = configureStore({
     shopProducts: shoppingProductsSlice,
     shopCart: shoppingCartSlice,
     shopAddress: shoppingAddressSlice,
+    shopOrder: shoppingOrderSlice,
   },
 });
 
