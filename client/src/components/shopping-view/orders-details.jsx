d6eaecdc455adad9742823d19c00caa337056682
Nguyễn Kiều Anh Quân <168531710+CharleSs0933@users.noreply.github.com>
2024-10-29 01:27:33 +0700

    order list page

--- client/src/components/shopping-view/orders-details.jsx
+++ client/src/components/shopping-view/orders-details.jsx
@@ -1,27 +1,48 @@
+import { useSelector } from "react-redux";
+import { Badge } from "../ui/badge";
 import { DialogContent } from "../ui/dialog";
 import { Label } from "../ui/label";
 import { Separator } from "../ui/separator";
 
-const ShoppingOrdersDetailsView = () => {
+const ShoppingOrdersDetailsView = ({ orderDetails }) => {
+  const { user } = useSelector((state) => state.auth);
   return (
     <DialogContent className="sm:max-w-[600px]">
       <div className="grid gap-6">
         <div className="grid gap-2">
           <div className="flex mt-6 items-center justify-between">
             <p className="font-medium">Order ID</p>
-            <Label>123456</Label>
+            <Label>{orderDetails?._id}</Label>
           </div>
           <div className="flex mt-2 items-center justify-between">
             <p className="font-medium">Order Date</p>
-            <Label>21/10/2024</Label>
+            <Label>{orderDetails?.orderDate.split("T")[0]}</Label>
           </div>
           <div className="flex mt-2 items-center justify-between">
             <p className="font-medium">Order Price</p>
-            <Label>$1000</Label>
+            <Label>${orderDetails?.totalAmount}</Label>
+          </div>
+          <div className="flex mt-2 items-center justify-between">
+            <p className="font-medium">Payment Method</p>
+            <Label>{orderDetails?.paymentMethod}</Label>
+          </div>
+          <div className="flex mt-2 items-center justify-between">
+            <p className="font-medium">Payment Status</p>
+            <Label>{orderDetails?.paymentStatus}</Label>
           </div>
           <div className="flex mt-2 items-center justify-between">
             <p className="font-medium">Order Status</p>
-            <Label>In Process</Label>
+            <Label>
+              <Badge
+                className={`py-1 px-3 ${
+                  orderDetails?.orderStatus === "confirmed"
+                    ? "bg-green-500"
+                    : "bg-black"
+                } `}
+              >
+                {orderDetails?.orderStatus}
+              </Badge>
+            </Label>
           </div>
         </div>
         <Separator />
@@ -29,10 +50,18 @@ const ShoppingOrdersDetailsView = () => {
           <div className="grid gap-2">
             <div className="font-medium">Order Details</div>
             <ul className="grid gap-3">
-              <li className="flex items-center justify-between">
-                <span>Product One</span>
-                <span>$100</span>
-              </li>
+              {orderDetails?.cartItems &&
+                orderDetails?.cartItems.length > 0 &&
+                orderDetails?.cartItems.map((item) => (
+                  <li
+                    className="flex items-center justify-between"
+                    key={item.productId}
+                  >
+                    <span>Title: {item.title}</span>
+                    <span>Quantity: {item.quantity}</span>
+                    <span>Price: ${item.price}</span>
+                  </li>
+                ))}
             </ul>
           </div>
         </div>
@@ -40,12 +69,12 @@ const ShoppingOrdersDetailsView = () => {
           <div className="grid gap-2">
             <div className="font-medium">Shipping Info</div>
             <div className="grid gap-0.5 text-muted-foreground">
-              <span>John Doe</span>
-              <span>Address</span>
-              <span>City</span>
-              <span>Pincode</span>
-              <span>Phone</span>
-              <span>Notes</span>
+              <span>{user.userName}</span>
+              <span>{orderDetails?.addressInfo.address}</span>
+              <span>{orderDetails?.addressInfo.city}</span>
+              <span>{orderDetails?.addressInfo.phone}</span>
+              <span>{orderDetails?.addressInfo.pincode}</span>
+              <span>{orderDetails?.addressInfo.notes}</span>
             </div>
           </div>
         </div>
--- client/src/store/shop/order-slice/index.js
+++ client/src/store/shop/order-slice/index.js
@@ -5,6 +5,8 @@ const initialState = {
   approvalURL: null,
   isLoading: false,
   orderId: null,
+  orderList: [],
+  orderDetails: null,
 };
 
 const apiUrl = import.meta.env.VITE_API_URL;
@@ -34,10 +36,32 @@ export const capturePayment = createAsyncThunk(
   }
 );
 
+export const getAllOrdersByUserId = createAsyncThunk(
+  "order/getAllOrdersByUserId",
+  async (userId) => {
+    const result = await axios.get(`${apiUrl}/api/shop/order/list/${userId}`);
+
+    return result?.data;
+  }
+);
+
+export const getOrderDetails = createAsyncThunk(
+  "order/getOrderDetails",
+  async (id) => {
+    const result = await axios.get(`${apiUrl}/api/shop/order/details/${id}`);
+
+    return result?.data;
+  }
+);
+
 const shoppingOrderSlice = createSlice({
   name: "shoppingOrder",
   initialState,
-  reducers: {},
+  reducers: {
+    resetOrderDetails: (state, action) => {
+      state.orderDetails = null;
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(createNewOrder.pending, (state) => {
@@ -56,8 +80,32 @@ const shoppingOrderSlice = createSlice({
         state.isLoading = false;
         state.approvalURL = null;
         state.orderId = null;
+      })
+      .addCase(getAllOrdersByUserId.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getAllOrdersByUserId.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.orderList = action.payload.data;
+      })
+      .addCase(getAllOrdersByUserId.rejected, (state, action) => {
+        state.isLoading = false;
+        state.orderList = [];
+      })
+      .addCase(getOrderDetails.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getOrderDetails.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.orderDetails = action.payload.data;
+      })
+      .addCase(getOrderDetails.rejected, (state, action) => {
+        state.isLoading = false;
+        state.orderDetails = null;
       });
   },
 });
+
+export const { resetOrderDetails } = shoppingOrderSlice.actions;
 
 export default shoppingOrderSlice.reducer;
