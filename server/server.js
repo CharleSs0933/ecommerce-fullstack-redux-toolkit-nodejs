c930a5364a04cdd802d64df5a4b8193cdffb4084
Nguyễn Kiều Anh Quân <168531710+CharleSs0933@users.noreply.github.com>
2024-10-30 00:13:15 +0700

    api for admin orders

--- server/server.js
+++ server/server.js
@@ -4,8 +4,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+
 const authRouter = require("./routes/auth/auth-routes");
+
 const adminProductsRouter = require("./routes/admin/product-routes");
+const adminOrdersRouter = require("./routes/admin/product-routes");
+
 const shopProductsRouter = require("./routes/shop/product-routes");
 const shopCartRouter = require("./routes/shop/cart-routes");
 const shopAddressRouter = require("./routes/shop/address-routes");
@@ -38,7 +42,10 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
+
 app.use("/api/admin/products", adminProductsRouter);
+app.use("/api/admin/orders", adminOrdersRouter);
+
 app.use("/api/shop/products", shopProductsRouter);
 app.use("/api/shop/cart", shopCartRouter);
 app.use("/api/shop/address", shopAddressRouter);
